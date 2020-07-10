81fc00af50c1ebfc59abb3573876cff4adbee4e6
AnthonyTC89 <ptonyptc@gmail.com>
2020-07-10 17:25:59 -0500

    some validations added to the model Stock

--- src/Models/Stock.js
+++ src/Models/Stock.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
 const StockSchema = new Schema({
-  name: { type: String, required: true, unique: true },
-  currentPrice: { type: Number, required: true },
+  name: { type: String, required: true, unique: true, minlength: 4 },
+  currentPrice: { type: Number, required: true, min: 0 },
   status: { type: Boolean, required: true, default: true },
 }, { collection: 'stocks', timestamps: { updatedAt: 'lastUpdate' } });
 
