572112859c2714f95ebd9e1700a4a2803ca05727
AnthonyTC89 <ptonyptc@gmail.com>
2020-07-14 21:42:47 -0500

    Footer added

--- src/Containers/Home.js
+++ src/Containers/Home.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import StockList from '../Components/StockList';
+import Footer from '../Components/Footer';
 
 const Home = () => (
-  <StockList />
+  <>
+    <StockList />
+    <Footer />
+  </>
 );
 
 export default Home;
